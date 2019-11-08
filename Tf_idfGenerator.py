83f9078c15ed24a8c5eaed667219b64448a4854c
U-LAPTOP-DELL\Andrew Munoz <munozandrew47@gmail.com>
2019-11-07 17:38:29 -0700

    added counter

--- Tf_idfGenerator.py
+++ Tf_idfGenerator.py
@@ -1,21 +1,23 @@
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-import os
 import json
 
 ps = PorterStemmer()
 inverted_index = {}
+DocCount=0
 with open('dataTest.json') as json_file:
     data = json.load(json_file)
     for entry in data:
+        DocCount+=1
         docName = entry["ID"]
         text = entry["Title"] + " " + entry["Author"] +" "+ entry["Type"]+ " "+entry["Platform"]
         terms = word_tokenize(text)
         for term in terms:
-            if ps.stem(term) not in inverted_index:
-                inverted_index[ps.stem(term)] = "d" + str(docName)
-            elif str(docName) not in inverted_index[ps.stem(term)]:
-                inverted_index[ps.stem(term)] = inverted_index[ps.stem(term)] + ", d" + str(docName)
+            term = ps.stem(term)
+            if term not in inverted_index:
+                inverted_index[term] = "d" + str(docName)
+            elif str(docName) not in inverted_index[term]:
+                inverted_index[term] = inverted_index[term] + ", d" + str(docName)
         print(docName)
         print(text)
 print(inverted_index)
