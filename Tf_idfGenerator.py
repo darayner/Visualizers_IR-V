d49d3595fb47a8a4a8bd9f9456ef5c4fe87ca632
U-LAPTOP-DELL\Andrew Munoz <munozandrew47@gmail.com>
2019-11-07 17:48:05 -0700

    Inverted Index made

--- Tf_idfGenerator.py
+++ Tf_idfGenerator.py
@@ -15,9 +15,10 @@ with open('dataTest.json') as json_file:
         for term in terms:
             term = ps.stem(term)
             if term not in inverted_index:
-                inverted_index[term] = "d" + str(docName)
-            elif str(docName) not in inverted_index[term]:
-                inverted_index[term] = inverted_index[term] + ", d" + str(docName)
-        print(docName)
-        print(text)
+                inverted_index[term] = {}
+                inverted_index[term][docName]= 1
+            elif docName not in inverted_index[term]:
+                inverted_index[term][docName] = 1
+            elif docName in inverted_index[term]:
+                inverted_index[term][docName] += 1
 print(inverted_index)
